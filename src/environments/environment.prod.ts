8cc6920013aa3b54eb65bdb3c43fda3dedf3a78d
Vitor Alves <vitor.alves@helpper.com>
2022-05-05 23:51:56 -0300

    Adicionando environments no projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
   production: true
 };
+
+export const SpotifyConfiguration = {
+  clientId: 'efb98fe2ef7a471597dcfa58b4a52a23',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'https://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,21 @@ export const environment = {
   production: false
 };
 
+export const SpotifyConfiguration = {
+  clientId: 'efb98fe2ef7a471597dcfa58b4a52a23',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'https://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
